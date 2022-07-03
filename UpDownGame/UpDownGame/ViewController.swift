9827db9cd83682055665bb276e1d8a4caefa6679
LeeJoEun-01 <joeun1005@naver.com>
2022-07-03 11:29:35 +0900

    Create Project 2

---
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 이조은 on 2022/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//  스토리에 있는 요소와 연결하는 어노테이션
+    @IBOutlet weak var slider: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+//  이벤트에 반응하는 액션
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        print("touch up reset button")
+    }
+
+}
+
