f04b5ac584bfe362e9b3a1beb6fe2191c3a9f9f8
LeeJoEun-01 <joeun1005@naver.com>
2022-07-06 22:13:07 +0900

    Add Project Auto Layout...

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        slider.setThumbImage(#imageLiteral(resourceName: "yagom_symbol"), for: .normal)
     }
 
 //  이벤트에 반응하는 액션
