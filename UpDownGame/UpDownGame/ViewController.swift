5cd0a86c284eb43cb9212bf998304400b06cd83f
LeeJoEun-01 <joeun1005@naver.com>
2022-07-10 23:04:32 +0900

    Add Project ~Chapter4

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -9,14 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomValue: Int = 0
+    var tryCount: Int = 0
+    
 //  스토리에 있는 요소와 연결하는 어노테이션
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         slider.setThumbImage(#imageLiteral(resourceName: "yagom_symbol"), for: .normal)
+        reset()
     }
 
 //  이벤트에 반응하는 액션
@@ -30,6 +38,21 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
         print("touch up reset button")
+        reset()
+    }
+    
+    func reset() {
+        print("reset!")
+        randomValue = Int.random(in: 0...30)
+        print(randomValue)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
     }
 
 }
